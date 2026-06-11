3f6680771bc0e63ac9f6cbba26c0db279c918b72
yulun7030 <yulun7030@gmail.com>
2026-06-11 23:12:09 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  // 💡 關鍵：必須加上這行，兩邊都要有斜線，名字要跟你的 GitHub 專案名稱一模一樣！
+  base: '/0611-/', 
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
